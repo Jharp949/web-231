b3ead977bcc0b70540d0b2b6de4f30766cf2c04b
James Harper <jharp949@gmail.com>
2023-04-30 11:57:09 -0700

    Changes to be committed: 	modified:   global.css 	modified:   week-7/harper-calculator.css 	modified:   week-7/harper-calculator.html 	modified:   week-7/harper-calculator.js

--- week-7/harper-calculator.js
+++ week-7/harper-calculator.js
@@ -16,7 +16,7 @@ function multiply(multiplyOne, multiplyTwo) {
   }
   //variable that multiplies the two parameters together.
   const product = multiplyOne * multiplyTwo;
-  //variable product returned as a result to executing the function.
+  //variable sum returned as a result to executing the function.
   return product;
 }
 //Checks if the function is operating properly.
@@ -34,40 +34,51 @@ function divide(divideOne, divideTwo) {
     throw alert('inputs must be numbers.');
     //Additional check to ensure that neither of the numbers are negative.
   } else if (divideOne < 0 || divideTwo < 0 === true) {
-    //Alert thrown is
+    //Alert thrown if either condition is true.
     throw alert('inputs must be valid numbers.');
   }
-
+  //variable that divides the two parameters.
   const division = divideOne / divideTwo;
+  //variable sum returned as a result to executing the function.
   return division;
 }
+//Checks if the function is operating properly.
 //console.log(divide(28, 7));
 
+//Function that executes multiply() then assigns it to a HTML element.
 function displayMultiplyResults() {
+  //Variable derived from the user input HTML id multInputOne.
   const multValueOne = document.getElementById('multInputOne').value;
+  //Variable derived from the user input HTML id multInputTwo.
   const multValueTwo = document.getElementById('multInputTwo').value;
+  //Ensures the input is in number form for the function parameter to operate.
   parseFloat(multValueOne);
   parseFloat(multValueTwo);
-
+  //try statement that executes multiply(). Then assigns the value to the span with an id of mul-results.
   try {
     const sum = multiply(multValueOne, multValueTwo);
     document.getElementById('mul-results').innerHTML = multValueOne + ' * ' + multValueTwo + ' = ' + sum;
   }
+  //Logs any errors that may occur when the function is executed.
   catch(err) {
     console.log(err);
   }
 }
-
+//Function that executes divide() then assigns it to a HTML element.
 function displayDivideResults() {
+  //Variable derived from the user input HTML id divInputOne.
   const divValueOne = document.getElementById("divInputOne").value;
+  //Variable derived from the user input HTML id divInputTwo.
   const divValueTwo = document.getElementById("divInputTwo").value;
+  //Ensures the input is in number form for the function parameter to operate.
   parseFloat(divValueOne);
   parseFloat(divValueTwo);
-
+  //try statement that executes divide(). Then assigns the value to the span with an id of div-results.
   try {
     const sum = divide(divValueOne, divValueTwo);
     document.getElementById('div-results').innerHTML = divValueOne + ' / ' + divValueTwo + ' = ' + sum;
   }
+  //Logs any errors that may occur when the function is executed.
   catch(err) {
     console.log(err);
   }
