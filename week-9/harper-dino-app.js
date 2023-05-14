60c1b05b0476be77113ab66dd69fa7076af26c95
James Harper <jharp949@gmail.com>
2023-05-13 22:17:09 -0700

    Changes to be committed: 	modified:   week-9/harper-dino-app.css 	modified:   week-9/harper-dino-app.html 	modified:   week-9/harper-dino-app.js

--- week-9/harper-dino-app.js
+++ week-9/harper-dino-app.js
@@ -3,40 +3,86 @@
 //Date: 5/12/2023
 //Description: Web 231 - Week 9 - Assignment 9.2
 
+//Defines a parent class and gives it one property called name. Contains a method roar() which returns a string
+//concatenated with the user's selected radio button choice.
 class Gigantosaurus {
   constructor(name) {
     this.name = name;
   }
   roar() {
-    return console.log(`${this.name} says: rooooooar`);
+    return `${this.name} says: rooooooar`;
   }
 }
 
-const gig = new Gigantosaurus('Gigantosaurus');
-console.log(gig.roar());
-
+//Child class of Gigantosaurus. Uses the same format for the name property. Changes the method name to growl()
+//which also has a different returned string.
 class Tyrannosaurus extends Gigantosaurus {
   constructor(name) {
     super(name);
-    this.name = name;
   }
   growl() {
-    return console.log(`${this.name} says: groooooowl`);
+    return `${this.name} says: groooooowl`;
   }
 }
 
-const tyr = new Tyrannosaurus('Tyrannosaurus');
-console.log(tyr.growl());
-
+//Child class of Tyrannosaurus. Uses the same format for the name property. Changes the method name to growl()
+//which also has a different returned string.
 class Velociraptor extends Tyrannosaurus {
   constructor(name) {
     super(name);
-    this.name = name;
   }
   awk() {
-    return console.log(`${this.name} says: awk awk awwwwwwk`);
+    return `${this.name} says: awk awk awwwwwwk`;
   }
 }
 
-const vel = new Velociraptor('Velociraptor');
-console.log(vel.awk());
+//The three following variables are assigned to the method for each preceding class.
+const gigantosaurus = new Gigantosaurus('Alfred').roar();
+const tyrannosaurus = new Tyrannosaurus('Georgie').growl();
+const velociraptor = new Velociraptor('Suzy').awk();
+//Checks output of the preceding three variables.
+//console.log(gigantosaurus);
+//console.log(tyrannosaurus);
+//console.log(velociraptor);
+
+
+//function linked to the HTML document button via an "onclick" event.
+function display() {
+
+  //retrieves all user selectable options of inputs named choice.
+  const choice = document.querySelectorAll('input[name="dinoChoice"]');
+  //empty variable to hold a returned valye from the following loop.
+  let radioSelected = '';
+  //Loops through each radio button to see which one is selected.
+  for (let radioButton of choice) {
+    if (radioButton.checked) {
+      //Adds the checked radio button value to the variable radioSelected.
+      radioSelected += radioButton.value;
+      break;
+    }
+  }
+
+  //Switch statement that assigns the selected method to the HTML id "results". If none of the values are
+  //selected, then an alert message to make a selection is returned.
+  switch (radioSelected) {
+
+    case 'gigantosaurus':
+      document.getElementById('results').innerHTML = gigantosaurus;
+
+    break;
+
+    case 'tyrannosaurus':
+      document.getElementById('results').innerHTML = tyrannosaurus;
+
+    break;
+
+    case 'velociraptor':
+      document.getElementById('results').innerHTML = velociraptor;
+
+    break;
+
+    default:
+
+        alert('Invalid selection, please try again!');
+  }
+}
